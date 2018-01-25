3cb7393af531ea24a7ee613c76d8ce10363d040c
Ajmal Nabil <ajmalnabiljamal@gmail.com>
2018-01-25 13:24:56 +0800

    Implement basic User login using Session with tests and implement basic logout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  include SessionsHelper
 
   def hello
     render html:"Hello world, this is PropMan!"
   end
-  
+
 end
